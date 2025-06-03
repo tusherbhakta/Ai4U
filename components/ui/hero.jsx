45322bcec420f688d934b8735e35186dfa4e8c06
tusherbhakta <tusheranta@gmail.com>
2025-06-03 13:57:30 +0530

    feat: enhance HeroSection with scroll-based image transformation effects

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -1,9 +1,29 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './button'
 import Image from 'next/image'
 
 const HeroSection = () => {
+
+    const ImageRef = useRef(null);
+    useEffect(() => {
+        const imageElement = ImageRef.current;
+        const handleScroll = () =>{ 
+            
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+            if(scrollPosition > scrollThreshold) {
+                imageElement.classList.add('scrolled');
+            }
+            else{
+                imageElement.classList.remove('scrolled');
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <section className='w-full pt-36 md:pt-48 pb-10'>
             <div className='space-y-6 text-center' >
@@ -22,8 +42,8 @@ const HeroSection = () => {
                         <Button size='lg' className="px-8" variant="outline">Get Started</Button>
                     </Link>
                 </div>
-                <div>
-                    <div>
+                <div className='hero-image-wrapper mt-5 md:mt-0'>
+                    <div ref={ImageRef} className='hero-image '>
                         <Image
                             src={'/banner.png'} width={1280} height={720} alt='Banner AI4U' className='rounded-lg shadow-2xl border mx-auto' priority
                         />
